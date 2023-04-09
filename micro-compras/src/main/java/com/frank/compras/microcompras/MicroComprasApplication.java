0ff1047b6ea32bbf335da26e1b2b053b94a70673
Francisco F. Cabanillas Yali <frank97-1@outlook.com>
2023-04-09 00:02:37 -0500

    load balancer

--- micro-compras/src/main/java/com/frank/compras/microcompras/MicroComprasApplication.java
+++ micro-compras/src/main/java/com/frank/compras/microcompras/MicroComprasApplication.java
@@ -2,8 +2,10 @@ package com.frank.compras.microcompras;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroComprasApplication {
 
 	public static void main(String[] args) {
